259d1ea2ca4ae0f76c6a2f56bcb407707dedc919
Henri Grant <123312985+WCMHGG@users.noreply.github.com>
2023-01-30 08:49:36 -0500

    used switch case for units

--- areacircle/src/areacircle.cpp
+++ areacircle/src/areacircle.cpp
@@ -1,19 +1,29 @@
-//============================================================================
-// Name        : areacircle.cpp
-// Author      : Henri Grant
-// Version     :
-// Copyright   : https://creativecommons.org/licenses/by/4.0/
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
 	int radius;
+    int u_unit;
+    int area;
+    string unit;
 	cout << "Please input the circle's radius" << endl;
 	cin >> radius; // user input integer radius
-	int area = 3.14 * radius * radius; // area calculation based off of user input
-	cout << area << endl; // output calculation
+    cout << "Press 1 or 2 for these measurement units" << endl << "1: cm  2: mm" << endl;
+    cin >> u_unit; // user input what unit type
+    switch (u_unit)
+    {
+        case 1:
+            unit = "cm";
+            break;
+        case 2: 
+            unit = "mm";
+            break;
+        default:
+            unit = "inch(es)";
+    }
+
+	area = 3.14 * radius * radius; // area calculation based off of user input
+	cout << "The area is:" << endl << area << " " << unit << endl; // output calculation
 	return 0;
 }
